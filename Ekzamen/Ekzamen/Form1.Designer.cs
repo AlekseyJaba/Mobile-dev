4b5293b8ac834947f4b457dff012a5502384549c
Volgin Aleksey <aleksejvolgin894@gmail.com>
2024-01-15 16:14:30 +0300

    Ekzamen

---
+++ Ekzamen/Ekzamen/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Ekzamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelTimeScoreLevel;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelLevel;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTimeScoreLevel = new System.Windows.Forms.Panel();
+            this.labelLevel = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelTimeScoreLevel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTimeScoreLevel
+            // 
+            this.panelTimeScoreLevel.Controls.Add(this.labelLevel);
+            this.panelTimeScoreLevel.Controls.Add(this.labelScore);
+            this.panelTimeScoreLevel.Controls.Add(this.labelTime);
+            this.panelTimeScoreLevel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTimeScoreLevel.Location = new System.Drawing.Point(0, 0);
+            this.panelTimeScoreLevel.Name = "panelTimeScoreLevel";
+            this.panelTimeScoreLevel.Size = new System.Drawing.Size(782, 50);
+            this.panelTimeScoreLevel.TabIndex = 0;
+            // 
+            // labelLevel
+            // 
+            this.labelLevel.AutoSize = true;
+            this.labelLevel.Location = new System.Drawing.Point(250, 18);
+            this.labelLevel.Name = "labelLevel";
+            this.labelLevel.Size = new System.Drawing.Size(46, 16);
+            this.labelLevel.TabIndex = 2;
+            this.labelLevel.Text = "Level: ";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(120, 18);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(49, 16);
+            this.labelScore.TabIndex = 1;
+            this.labelScore.Text = "Score: ";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(12, 18);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(44, 16);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "Time: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(696, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "скрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panelTimeScoreLevel);
+            this.Name = "Form1";
+            this.Text = "Scoreboard";
+            this.panelTimeScoreLevel.ResumeLayout(false);
+            this.panelTimeScoreLevel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ekzamen/Ekzamen/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Label = System.Windows.Forms.Label;
+
+namespace Ekzamen
+{
+
+    public partial class Form1 : Form
+    {
+        private Label label = new Label();
+        private int score = 0;
+        private int level = 1;
+        private Timer timer;
+        private Random random = new Random();
+        private Label[,] labels = new Label[3, 4];
+        private int[,] num = new int[3, 4];
+        private int _heigth = 3;
+        private int _width = 4;
+        private int box_size = 80;
+        private int CountChoise = 0;
+        private bool flag = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            generateMap();
+            timer.Interval = 1000; // Интервал таймера в миллисекундах (1 секунда)
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            UpdateTimeLabel();
+            UpdateScoreLabel();
+            UpdateLevelLabel();
+        }
+
+        private void generateMap()
+        {
+            int y = 0;
+
+            for (int i = 0; i< _heigth * box_size; i+=box_size)
+            {
+                int x = 0;
+                for (int j = 0; j < _width*box_size; j+=box_size)
+                {
+                    Label pic = new Label();
+                    pic.BackColor = Color.Green;
+                    pic.Text = "";
+                    pic.Location = new Point(100+j, 100 + i);
+                    pic.Size = new Size(50, 30);
+                    pic.MouseClick += label_Click;
+                    num[y, x] = 0;
+                    labels[y, x] = pic;
+                    this.Controls.Add(pic);
+
+                    x++;
+                }
+                y++;
+            }
+            DoNumber(labels);
+        }
+
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+            // Получаем ссылку на Label, на которую был сделан клик
+            Label clickedLabel = (Label)sender;
+
+            clickedLabel.BackColor = Color.Red;
+            CountChoise +=1;
+            clickedLabel.Text = CountChoise.ToString();
+            for (int i = 0; i<3; i++)
+            {
+                for (int j = 0; j< 4; j++)
+                {
+                    if (clickedLabel.Name == labels[i, j].Name)
+                    {
+                        // if (labels[i,j].Text== )
+                    }
+                }
+            }
+            // Вызываем функцию для изменения цвета Label
+        }
+        private void DoNumber(Label[,] labels)
+        {
+            random = new Random();
+            //int n = random.Next(1, 9);
+
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                int col = random.Next(0, 3);
+                int row = random.Next(0, 2);
+
+                MessageBox.Show($"{col}, {row}");
+                if (labels[col, row].Text != "")
+                {
+                    i -= 1;
+                }
+                else
+                {
+                    labels[col, row].Text = i.ToString();
+                    num[col, row] = i;
+                }
+
+            }
+        }
+
+
+        //private void CreateLabelManager()
+        //{
+        //    labelArray = new Label[,]
+        //{
+        //    { label1, label2, label3, label4 },
+        //    { label5, label6, label7, label8 },
+        //    { label9, label10, label11, label12 }
+        //};
+
+        //    // Создаем экземпляр LabelManager с массивом labelArray
+        //    labelManager = new LabelManager(labelArray);
+        //}
+
+
+        private void UpdateLevelLabel()
+        {
+            labelLevel.Text = "Level: " + level.ToString();
+        }
+
+        private void UpdateScoreLabel()
+        {
+            labelScore.Text = "Score: " + score.ToString();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTimeLabel();
+
+        }
+
+
+        private void UpdateTimeLabel()
+        {
+            labelTime.Text = "Time: " + DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    labels[i, j].Text = "";
+                }
+            }
+        }
+    }
+
+}
